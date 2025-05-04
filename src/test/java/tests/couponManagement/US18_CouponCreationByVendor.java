0f6c10ae1447592e1bfb79f6cafb82a8686c89d2
Esma Isiyok <esmaisiyok91@gmail.com>
2025-05-04 19:36:47 +0200

    deneme 04

--- src/test/java/tests/couponManagement/US18_CouponCreationByVendor.java
+++ src/test/java/tests/couponManagement/US18_CouponCreationByVendor.java
@@ -3,7 +3,8 @@ package tests.couponManagement;
 public class US18_CouponCreationByVendor {
     public static void main(String[] args) {
         System.out.println("deneme 01");
-        System.out.println("deneme 03");
+        System.out.println("deneme 04");
 
     }
 }
+
